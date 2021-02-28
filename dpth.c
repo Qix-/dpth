a612e0435cd629bcf919bb4579fb4a859a2a8bae
Josh Junon <junon@wavetilt.com>
2021-02-28 05:35:47 +0100

    ABANDON SHIP. Read rest of commit message for details.
    So, technically this could work. And it does, mostly.
    However, I decided to stop for two reasons:
    
    1. I didn't really *need* this tool. I just had the idea for it
       and it seemed like it'd be fun to implement.
    
    2. It only works assuming the child process doesn't change its
       PATH variable, which since you can't guarantee that,
       means that it could (in theory) completely bypass
       the host program's PATH variable and confuse the user.
    
       I would have to, ultimately, hook every single possible
       thing (including libc, Go programs, etc.) in order to
       detect setenv() calls or exec*p() calls (or whatever
       that runtime's thing happens to be called) to be able
       to show a diagnostic message reliably.
    
    It's not something urgent or useful enough for this to really
    be worth it to continue, and thus I am committing this here
    and archiving the repo.

--- dpth.c
+++ dpth.c
@@ -6,23 +6,225 @@
 
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include <assert.h>
+#include <limits.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <setjmp.h>
 
 #define DPTH_COUNTOF(x) (sizeof((x))/sizeof((x)[0]))
 
+static pid_t child_pid = 0;
+static jmp_buf jump_env;
+
+static int const signals_to_ignore[] = {
+#ifdef SIGHUP
+	SIGHUP,
+#endif
+#ifdef SIGQUIT
+	SIGQUIT,
+#endif
+
+	SIGINT,
+	SIGTERM
+};
+
+static char * dpth_strdup(const char *original) {
+	size_t len = strlen(original) + 1;
+	char *buf = malloc(len);
+	return buf == NULL
+		? NULL
+		: memcpy(buf, original, len);
+}
+
+static void handle_SIGCHLD(int signum) {
+	(void) signum;
+	assert(signum == SIGCHLD);
+	longjmp(jump_env, 1);
+}
+
+static int setsig_or_perror(int sig, int restart, void (*handler)(int)) {
+	struct sigaction action;
+	action.sa_flags = SA_NOCLDSTOP | (restart ? SA_RESTART : 0);
+	sigfillset(&action.sa_mask);
+	action.sa_restorer = 0;
+	action.sa_handler = handler;
+
+	if (sigaction(sig, &action, NULL) == -1) {
+		perror("dpth: sigaction()");
+		return 1;
+	}
+
+	return 0;
+}
+
+static char * getenv_child(const char *name) {
+
+}
+
+/* return value MUST be free()'d if not NULL! */
+static char * get_PATH(void) {
+	if (child_pid == 0) {
+		/*
+			we haven't set up a child, but for some reason (perhaps
+			another program) we're being asked to grab its PATH
+			variable.
+
+			simply ignore the request.
+		*/
+		return NULL;
+	}
+
+	char *pathenv = getenv_child("PATH");
+
+	if (pathenv == NULL) {
+		errno = 0;
+		size_t len = confstr(_CS_PATH, NULL, 0);
+
+		if (len == 0) {
+			if (errno == EINVAL) {
+				/* TODO use diagnostic instead of this */
+				fputs("@@@ DPTH @@@: WARNING: confstr(_CS_PATH) indicated it didn't understand the variable\n", stderr);
+			}
+		} else {
+			pathenv = malloc(len);
+
+			if (pathenv == NULL) {
+				/* TODO use defines for the diagnostic prefix, and use them in the diagnostic function as well as here to keep things consistent */
+				/* TODO alternatively, just use strerror() and pass to diagnostic */
+				perror("@@@ DPTH @@@: WARNING: malloc()\n");
+			} else {
+
+				/* guaranteed to succeed */
+#			ifndef NDEBUG
+				size_t check_return =
+#			endif
+				confstr(_CS_PATH, pathenv, len);
+#			ifndef NDEBUG
+				assert(check_return == len);
+#			endif
+			}
+		}
+	} else {
+		pathenv = dpth_strdup(pathenv);
+
+		if (pathenv == NULL) {
+			/* TODO use defines for the diagnostic prefix, and use them in the diagnostic function as well as here to keep things consistent */
+			/* TODO alternatively, just use strerror() and pass to diagnostic */
+			perror("@@@ DPTH @@@: WARNING: strdup() (via malloc())\n");
+		}
+	}
+
+	return pathenv;
+}
+
+/* return value MUST be free()'d if not NULL! */
+static char * try_stat_path(
+	const char *base,
+	size_t base_extent,
+	const char *partial,
+	size_t partial_extent,
+	struct stat *statbuf
+) {
+	if (partial_extent == 0) {
+		/* TODO maybe a diagnostic? */
+		return NULL;
+	}
+
+	int needs_slash = (*partial != '/');
+
+	/* construct the final path */
+	char *buf = malloc(base_extent + needs_slash + partial_extent + 1);
+	                 /*                  '/'                       NUL */
+
+	memcpy(buf, base, base_extent);
+	memcpy(buf + base_extent + needs_slash, partial, partial_extent);
+	if (needs_slash) buf[base_extent] = '/';
+	buf[base_extent + partial_extent + needs_slash] = 0;
+
+	fprintf(stderr, "TRY STAT: %s\n", buf);
+	if (stat(buf, statbuf) == -1) {
+		free(buf);
+		buf = NULL;
+	}
+
+	return buf;
+}
+
+/* return value MUST be free()'d if not NULL! */
+static char * resolve_path(const char *partial, struct stat *statbuf, int diagnostic) {
+	size_t partial_length;
+	size_t PATH_length;
+	char *PATH          = NULL;
+	char *resolved_path = NULL;
+	char *base_buffer   = NULL;
+
+	PATH = get_PATH();
+	if (PATH == NULL || (PATH_length = strlen(PATH)) == 0) {
+		/*
+			means an error; return NULL unconditionally,
+			without error, since that function has already
+			printed an error if it returned NULL.
+		*/
+		goto exit;
+	}
+
+	base_buffer = malloc(strlen(PATH));
+	if (base_buffer == NULL) {
+		/* TODO print diagnostic instead of this printf */
+		perror("@@@ DPTH @@@: WARNING: malloc()\n");
+		goto exit;
+	}
+
+	partial_length = strlen(partial);
+	if (partial_length) {
+
+	}
+
+	if (diagnostic) /* TODO print diagnostic */ (void)0;
+
+	for (size_t i = 0; i < PATH_length; i++) {
+
+	}
+
+	if (diagnostic) /* TODO print diagnostic */ (void)0;
+
+exit:
+	free(base_buffer);
+	free(PATH);
+	return resolved_path;
+}
+
+static int do_getattr(const char *path, struct stat *statbuf) {
+	char *resolved = resolve_path(path, statbuf, 1);
+
+	if (resolved == NULL) {
+		/* TODO print diagnostic instead of this hacked-on fprintf statement */
+		fprintf(stderr, "@@@ DPTH @@@: getattr(): %s: ENOENT\n", path);
+		return -ENOENT;
+	} else {
+		/* TODO print diagnostic instead of this hacked-on fprintf statement */
+		fprintf(stderr, "@@@ DPTH @@@: getattr(): %s: RESOLVED TO %s\n", path, resolved);
+		free(resolved);
+		return 0;
+	}
+}
+
 static const struct fuse_operations fuseops = {
+	.getattr = &do_getattr
 };
 
 int main(int argc, char *argv[]) {
 	int status;
 	pid_t pid;
-	struct fuse_chan *fuse_channel;
-	struct fuse *fuse_ctx;
+	struct fuse_chan *fuse_channel = NULL;
+	struct fuse *fuse_ctx = NULL;
 	char tmpdir_path[] = "/tmp/dpth-XXXXXX";
 
 	status = 1;
@@ -49,10 +251,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	{
-		const char *fuse_argv[] = {
-			"",
-			"-f"
-		};
+		static char * const fuse_argv[] = { "" };
 
 		struct fuse_args fuse_args = {
 			.allocated = 0,
@@ -74,7 +273,7 @@ int main(int argc, char *argv[]) {
 				Sorry for any headache this causes. Nothing I can do about
 				it without redesigning FUSE's frontend library.
 			*/
-			goto exit_dir;
+			goto exit_fuse;
 		}
 
 		fuse_ctx = fuse_new(
@@ -86,8 +285,22 @@ int main(int argc, char *argv[]) {
 		);
 
 		if (fuse_ctx == NULL) {
-			/* Same here; no error info. */
-			goto exit_fuse_channel;
+			/* Same here; no error info aside from what's printed to stderr. */
+			goto exit_fuse;
+		}
+	}
+
+	/*
+		Set up child signal; this gets destroyed during our exec*() call below anyway,
+		which is good because doing it inside the default case (parent process
+		branch) below would cause a race condition.
+	*/
+	if (setsig_or_perror(SIGCHLD, 0, &handle_SIGCHLD)) goto exit_fuse;
+	for (size_t i = 0; i < DPTH_COUNTOF(signals_to_ignore); i++) {
+		if (
+			setsig_or_perror(signals_to_ignore[i], 1, SIG_IGN)
+		) {
+			goto exit_fuse;
 		}
 	}
 
@@ -96,6 +309,15 @@ int main(int argc, char *argv[]) {
 			perror("dpth: fork()");
 			goto exit_fuse;
 		case 0:
+			for (size_t i = 0; i < DPTH_COUNTOF(signals_to_ignore); i++) {
+				if (
+					signal(signals_to_ignore[i], SIG_DFL) == SIG_ERR
+				) {
+					perror("dpth: signal()");
+					return -3;
+				}
+			}
+
 			if (setenv("PATH", tmpdir_path, 1) == -1) {
 				perror("dpth: setenv()");
 				return -2;
@@ -109,29 +331,41 @@ int main(int argc, char *argv[]) {
 			/* parent process */
 			int child_status = 0;
 
-			if (waitpid(pid, &child_status, 0) == -1) {
-				perror("dpth: waitpid()");
+			switch (setjmp(jump_env)) {
+			case 1:
+				/* child died */
+				if (waitpid(pid, &child_status, 0) == -1) {
+					perror("dpth: waitpid()");
+					/* fall-through to case 2 (kill child process and exit) */
+				} else {
+					status = WEXITSTATUS(child_status);
+					break;
+				}
 
+				/* intentional fallthrough */
+			case 2:
 				if (kill(pid, SIGKILL) == -1) {
 					perror("dpth: kill()");
 				}
 
-				goto exit_fuse;
-			}
+				status = -4;
 
-			status = WEXITSTATUS(child_status);
+				goto exit_fuse;
+			case 0:
+				fuse_loop(fuse_ctx);
+				/* should never return, and thus never hit here */
+				assert(!"FUSE loop returned prematurely");
+				longjmp(jump_env, 2);
+			}
 
 			break;
 		}
 	}
 
 exit_fuse:
-	assert(fuse_ctx != NULL);
-	fuse_destroy(fuse_ctx);
-exit_fuse_channel:
-	assert(fuse_channel != NULL);
-	fuse_unmount(tmpdir_path, fuse_channel);
-exit_dir:
+	if (fuse_channel) fuse_unmount(tmpdir_path, fuse_channel);
+	if (fuse_ctx) fuse_destroy(fuse_ctx);
+
 	if (rmdir(tmpdir_path) == -1) {
 		status = 1;
 		perror("dpth: rmdir()");
